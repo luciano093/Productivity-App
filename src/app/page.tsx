c0fd94cf56fcafff97ec67e122e14c4d0c0c6488
L <luparedes100@gmail.com>
2024-09-24 17:08:28 -0400

    add container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,31 @@ export default async function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#0bbfec] text-white">
         <Started />
+        <div className="flex flex-row bg-sky-800 w-full h-fit min-h-96">
+          <div className="h-full"></div>
+          <div className="flex flex-col min-h-full items-center p-6 [&_*]:py-2 m-auto">
+            <span className="font-semibold text-3xl">This is the title</span>
+            <span className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+            <div className="flex flex-row min-h-52 grid-flow-row">
+              <div className="flex flex-col max-w-fit">
+                <span className="font-semibold text-xl">Subheading</span>
+                <span className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+              </div>
+              <div className="flex flex-col max-w-fit">
+                <span className="font-semibold text-xl">Subheading</span>
+                <span className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+              </div>
+              <div className="flex flex-col max-w-fit">
+                <span className="font-semibold text-xl">Subheading</span>
+                <span className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+              </div>
+              <div className="flex flex-col max-w-fit">
+                <span className="font-semibold text-xl">Subheading</span>
+                <span className="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
   );
 }
