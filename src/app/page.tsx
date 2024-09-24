926e5d9515884ded43d2b6bf9f0aa47a908aae67
do262431@gmail.com <do262431@gmail.com>
2024-09-24 01:31:21 -0400

    Add NavMenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { getServerAuthSession } from "note/server/auth";
 import { api } from "note/trpc/server";
 import Started from "./_components/started";
+import Navbar from "./_components/navbar";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -9,7 +10,11 @@ export default async function Home() {
 
   return (
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#0bbfec] text-white">
+        <div className="top">
+        <p>This To-Do List helps you stay organized and get things done. Add tasks, set priorities, and check them off as you complete them. It’s a simple tool to keep track of what you need to do, all in one place.</p>
+        <Navbar />
         <Started />
+        </div>
       </main>
   );
 }
--- src/n/components/ui/navigation-menu.tsx
+++ src/n/components/ui/navigation-menu.tsx
@@ -3,7 +3,7 @@ import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu"
 import { cva } from "class-variance-authority"
 import { ChevronDown } from "lucide-react"
 
-import { cn } from "s/lib/utils"
+import { cn } from "../../lib/utils"
 
 const NavigationMenu = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Root>,
@@ -12,7 +12,7 @@ const NavigationMenu = React.forwardRef<
   <NavigationMenuPrimitive.Root
     ref={ref}
     className={cn(
-      "relative z-10 flex max-w-max flex-1 items-center justify-center",
+      "relative top-80 z-10 flex max-w-max flex-1 items-center justify-center",
       className
     )}
     {...props}
