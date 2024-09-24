f065dcc170a8bb852e889b5ffab9bc18ad838d78
do262431@gmail.com <do262431@gmail.com>
2024-09-24 00:55:24 -0400

    Add button

---
+++ src/app/_components/started.tsx
@@ -0,0 +1,9 @@
+import { Button } from "note/n/components/ui/button";
+
+export default function Started(){
+    return (
+        <div>
+        <Button variant="destructive" size='lg'>Getting Started</Button>
+        </div>
+    )
+}
